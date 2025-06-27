06e9b68acdf66c7e9faf0068793a251c50e03eb8
Juliaavrora <78406923+Juliaavrora@users.noreply.github.com>
2025-06-27 13:57:17 +0400

    Create __manifest__.py

---
+++ odoo/custom/addons/pos_cashback/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'POS Cashback Per Customer',
+    'version': '1.0',
+    'summary': 'Override loyalty grant with customer cashback rate',
+    'category': 'Point of Sale',
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/assets.xml',
+    ],
+    'assets': {
+        'point_of_sale.assets': [
+            '/pos_cashback/static/src/js/loyalty_cashback.js',
+        ],
+    },
+    'installable': True,
+    'auto_install': False,
+}
