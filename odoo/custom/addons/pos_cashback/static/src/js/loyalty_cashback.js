c7cac22452ccec67a5203e69bd8cd6030389aa85
Juliaavrora <78406923+Juliaavrora@users.noreply.github.com>
2025-06-27 15:04:58 +0400

    Update loyalty_cashback.js

--- odoo/custom/addons/pos_cashback/static/src/js/loyalty_cashback.js
+++ odoo/custom/addons/pos_cashback/static/src/js/loyalty_cashback.js
@@ -1,12 +1,12 @@
 /** @odoo-module **/
-console.log("✅ loyalty_cashback.js LOADED");
+console.log("✅ POS Cashback module loaded");
+
 import { PosGlobalState } from "@point_of_sale/app/store/pos_global_state";
 
 PosGlobalState.prototype._processData = async function (loadedData) {
     await this._super(loadedData);
 
     for (let partner of this.partners) {
-        // если есть поле cashback_rate, сохраняем его
         if (partner.cashback_rate !== undefined) {
             partner.loyalty_grant = partner.cashback_rate;
         }
