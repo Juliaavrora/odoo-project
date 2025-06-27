9476b00609a6770513a40f6d074ae13a858d491c
Juliaavrora <78406923+Juliaavrora@users.noreply.github.com>
2025-06-27 14:36:55 +0400

    Update loyalty_cashback.js

--- odoo/custom/addons/pos_cashback/static/src/js/loyalty_cashback.js
+++ odoo/custom/addons/pos_cashback/static/src/js/loyalty_cashback.js
@@ -1,5 +1,5 @@
 /** @odoo-module **/
-
+console.log("✅ loyalty_cashback.js LOADED");
 import { PosGlobalState } from "@point_of_sale/app/store/pos_global_state";
 
 PosGlobalState.prototype._processData = async function (loadedData) {
